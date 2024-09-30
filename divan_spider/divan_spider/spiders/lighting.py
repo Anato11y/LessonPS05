b75d468344b87f05da96c343e187e0c9265d0695
Anato11y <tol66@yandex.ru>
2024-10-01 01:59:50 +0800

    6

---
+++ divan_spider/divan_spider/spiders/lighting.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class LightingSpider(scrapy.Spider):
+    name = "lighting"
+    allowed_domains = ["divan.ru"]
+    start_urls = ["https://www.divan.ru/irkutsk/category/svetilniki"]
+
+    def parse(self, response):
+        divans = response.css('div._Ud0k')
+        for divan in divans:
+            yield {
+                 'name': divan.css('div.lsooF span::text').get(),
+                 'price': divan.css('div.pY3d2 span::text').get(),
+                 'url': divan.css('a').attrib['href']
+            }
